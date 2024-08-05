962e2d09293a930b37374a0f0e53234907ac859b
desgurpreet <des.gurpreetsingh@gmail.com>
2024-08-05 09:47:04 +0530

    build random password

---
+++ script.js
@@ -0,0 +1,17 @@
+const passwordBox = document.getElementById("password");
+function createPassword(length) {
+  const charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  passwordBox.value = password;
+  return password;
+}
+
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
